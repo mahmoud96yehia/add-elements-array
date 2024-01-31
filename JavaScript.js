3ac192c88dfc6081147b09a662a650c3e78beed4
Mahmoud Yahia <66545982+mahmoud96yehia@users.noreply.github.com>
2024-01-31 16:06:19 +0200

    Add files via upload

---
+++ JavaScript.js
@@ -0,0 +1,21 @@
+﻿
+var elem = [];
+var nextindex = 0;
+function addElement() {
+   // debugger;
+    elem[nextindex] = document.getElementById("write").value;
+    nextindex++;
+    document.getElementById("write").value = '';
+}
+function showElement() {
+
+
+    var result = '<ul>';
+    for (var i = 0; i < elem.length; i++) {
+
+        result += "<li>"+"Element "+ i +"= "+ elem[i] + "</li>";
+    }
+    result += '</ul>';
+    document.getElementById("show").innerHTML = result;
+
+}
